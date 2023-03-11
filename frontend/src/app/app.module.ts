def67eddbb0809fe52e3a9ce367bdef0ef70b98e
Justin Going <justin.j.going@gmail.com>
2023-03-11 11:50:27 -0800

    Login form created. Global styles not applying for some reason. Need to troubleshoot.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { VoiceControlService } from './voice-control.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,6 +9,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 
 const appRoutes: Routes = [{ path: '', component: LoginComponent }];
 
@@ -20,9 +26,15 @@ const appRoutes: Routes = [{ path: '', component: LoginComponent }];
 		MatIconModule,
 		MatButtonModule,
 		RouterModule,
+		MatCardModule,
+		MatFormFieldModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatInputModule,
 		RouterModule.forRoot(appRoutes),
+		MatTabsModule,
 	],
-	providers: [],
+	providers: [VoiceControlService],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,48 @@
+import { VoiceControlService } from './../voice-control.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+	selectedTabIndex = 0;
+	loginForm!: FormGroup;
+	registerForm!: FormGroup;
 
+	constructor(private fb: FormBuilder, private VoiceControlService: VoiceControlService) {}
+
+	ngOnInit() {
+		this.loginForm = this.fb.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required],
+		});
+
+		this.registerForm = this.fb.group({
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
+			username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9_-]{5,}$/)]],
+			password: [
+				'',
+				[
+					Validators.required,
+					Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+				],
+			],
+		});
+	}
+
+	login() {
+		if (this.loginForm.valid) {
+			this.VoiceControlService.sendLoginInfo(this.loginForm.value);
+		}
+	}
+
+	register() {
+		if (this.registerForm.valid) {
+			this.VoiceControlService.sendRegistrationInfo(this.registerForm.value);
+		}
+	}
 }
